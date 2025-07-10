8fc3e2804fee5b2f9cac34c60b4062a2c2f7e5b9
SajagIN <sajag1403@gmail.com>
2025-07-10 23:07:03 +0530

    global commands

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-require('dotenv').config({ quiet: true });
-const fs = require('fs');
-const path = require('path');
-const { Client, GatewayIntentBits } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
-const { TOKEN, CLIENT_ID, COMMANDS } = require('./utils/constants');
-const { handleButton } = require('./utils/quizFunctions');
+require("dotenv").config({ quiet: true });
+const fs = require("fs");
+const path = require("path");
+const { Client, GatewayIntentBits } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord.js");
+const { TOKEN, CLIENT_ID, COMMANDS } = require("./utils/constants");
+const { handleButton } = require("./utils/quizFunctions");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.commands = new Map();
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(f => f.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync(path.join(__dirname, "commands"))
+  .filter((f) => f.endsWith(".js"));
 
 for (const file of commandFiles) {
   const mod = require(`./commands/${file}`);
   client.commands.set(mod.data.name, mod);
 }
 
-client.once('ready', async () => {
-  const rest = new REST({ version: '10' }).setToken(TOKEN);
-  
-  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: COMMANDS });
+client.once("ready", async () => {
+  const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+  const response = await rest.put(Routes.applicationCommands(CLIENT_ID), {
+    body: COMMANDS,
+  });
+  console.log("Registered global commands:", response);
 
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (interaction.isChatInputCommand()) {
     const cmd = client.commands.get(interaction.commandName);
     if (cmd) await cmd.execute(interaction);
--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,25 @@
 module.exports = {
   TOKEN: process.env.DISCORD_BOT_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
-  QUIZ_TIMEOUT: 30000
+  QUIZ_TIMEOUT: 30000,
+  COMMANDS: [
+    {
+      name: 'quiz',
+      description: 'Starts an AI or not image quiz',
+      options: [
+        {
+          name: 'questions',
+          description: 'Number of questions (1-10)',
+          type: 4,
+          required: true,
+          min_value: 1,
+          max_value: 10
+        }
+      ]
+    },
+    {
+      name: 'unlimited_quiz',
+      description: 'Starts an unlimited AI or not image quiz'
+    }
+  ]
 };
